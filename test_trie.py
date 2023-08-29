5f2ff1827d5c7eab588ab8f4dec662cc511c72eb
xinlin-z <xinlin.zhang@hotmail.com>
2023-08-29 21:40:51 +0800

    add stress test

--- test_trie.py
+++ test_trie.py
@@ -2,6 +2,7 @@ import random
 from trie import Trie
 
 
+# basic test
 words = ['a','ab','abc','abcd','abcde','abcde12345','abc']
 t = Trie()
 for w in words:
@@ -33,6 +34,30 @@ for w in words2:
     t.remove(w)
     words.remove(w)
     assert sorted(t.startswith('')) == sorted(words)
+assert t.node_size == 0
+assert t.word_size == 0
+
+
+# stress test
+t = Trie()
+n = 100
+for i in range(n):
+    s = ''.join(str(random.randint(0,999)) for i in range(100))
+    t.insert(s)
+    assert t.query(s)
+words = t.startswith('')
+assert len(words) == t.word_size
+
+ks = ['1','2','3','4','5','6','7','8','9']
+for k in ks:
+    num = sum(1 for w in words if w.startswith(k))
+    assert num == len(t.startswith(k))
+
+random.shuffle(words)
+for i,w in enumerate(words):
+    t.remove(w)
+    assert t.query(w) is False
+    assert t.word_size == len(words)-i-1
 assert t.node_size == 0
 assert t.word_size == 0
 
